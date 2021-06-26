6486571d2ffb39ab989cd9aaad7ca42858b420a6
Samaresh Samanta <ss9261@srmist.edu.in>
2021-06-27 03:12:29 +0530

    added the nutrition

--- frontend/src/components/NutritionPage/ChangePlan/ChangePlan.js
+++ frontend/src/components/NutritionPage/ChangePlan/ChangePlan.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './ChangePlan.module.css'
 import food from '../../../images/nutrition/food2.png'
 
 const ChangePlan = ()=>{
+
+
+    const [type, setType] = useState("fatloss");
+    const [sort, setSort] = useState("veg");
+    const [times, setTimes] = useState(3);
+
+    const s={
+        border: "2px solid #5a64c5",
+        color: "#5a64c5"
+    }
+    const f={
+        border: "2px solid #bcb5b5",
+        color: "#000"
+    }
+
+
     return(
         <div className={Styles.majorContainer}>
             <div className={Styles.Header}>
@@ -14,15 +30,15 @@ const ChangePlan = ()=>{
                         <h3>1.Diet Type</h3>
                     </div>
                     <div className={Styles.cardCtn}>
-                        <div className={Styles.card}>
+                        <div className={Styles.card} style={(type.localeCompare("fatloss") === 0)? {"backgroundColor":"#1F2024"}:{"backgroundColor":"#5a64c5"}} onClick={()=>{setType("fatloss")}}>
                             <img src={food} />
                             <h3>Fat loose</h3>
                         </div>
-                        <div className={Styles.card}>
+                        <div className={Styles.card} style={(type.localeCompare("weightgain") === 0)? {"backgroundColor":"#1F2024"}:{"backgroundColor":"#5a64c5"} } onClick={()=>{setType("weightgain")}}>
                             <img src={food} />
                             <h3>Weight Gain</h3>
                         </div>
-                        <div className={Styles.card}>
+                        <div className={Styles.card} style={(type.localeCompare("keto") === 0)? {"backgroundColor":"#1F2024"}:{"backgroundColor":"#5a64c5"} } onClick={()=>{setType("keto")}}>
                             <img src={food} />
                             <h3>Keto Diet</h3>
                         </div>
@@ -30,43 +46,44 @@ const ChangePlan = ()=>{
                 </div>
                 <div className={Styles.filter}>
                     <div className={Styles.list}>
-                        <div className={Styles.head}>
+                        <div className={Styles.head1}>
                             <h3>2.Foods</h3>
                         </div>
-                        <div className={Styles.item}>
+                        <div className={Styles.item} style={(sort.localeCompare("veg") === 0)? s:f} onClick={()=>{setSort("veg")}}>
                             <h3>Vegeterian</h3>
                         </div>
-                        <div className={Styles.item}>
+                        <div className={Styles.item} style={(sort.localeCompare("nonveg") === 0)? s:f} onClick={()=>{setSort("nonveg")}}>
                             <h3>Non Vegeterian</h3>
                         </div>
-                        <div className={Styles.item}>
+                        <div className={Styles.item} style={(sort.localeCompare("egg") === 0)? s:f} onClick={()=>{setSort("egg")}}>
                             <h3>Eggeterian</h3>
                         </div>
                     </div>
                     <div className={Styles.list}>
-                        <div className={Styles.head}>
-                            <h3>2.Kcal Per Day</h3>
+                        <div className={Styles.head1}>
+                            <h3>2.Times Per Day</h3>
                         </div>
-                        <div className={Styles.item}>
-                            <h3>2500kcal</h3>
+                        <div className={Styles.item} style={(times == 3)? s:f} onClick={()=>{setTimes(3)}}>
+                            <h3>3</h3>
                         </div>
-                        <div className={Styles.item}>
-                            <h3>2500kcal</h3>
+                        <div className={Styles.item}  style={(times == 4)? s:f} onClick={()=>{setTimes(4)}}>
+                            <h3>4</h3>
                         </div>
-                        <div className={Styles.item}>
-                            <h3>2500kcal</h3>
-                        </div>
-                        <div className={Styles.item}>
-                            <h3>2500kcal</h3>
-                        </div>
-                        <div className={Styles.item}>
-                            <h3>2500kcal</h3>
+                        <div className={Styles.item}  style={(times == 5)? s:f} onClick={()=>{setTimes(5)}}>
+                            <h3>5</h3>
                         </div>
                     </div>
                 </div>
 
             </div>
-            
+            <div className={Styles.foot}>
+                <div className={Styles.button}>
+                    Custom diet
+                </div>
+                <div className={Styles.button}>
+                    create diet
+                </div>
+            </div>
 
         </div>
     )
